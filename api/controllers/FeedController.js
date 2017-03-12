ace08120fd0856f915ae1dcf3aa076a476f921c5
lzys90 <leongyzs@gmail.com>
2017-03-13 02:07:31 +0800

    Init repo

---
+++ api/controllers/FeedController.js
@@ -0,0 +1,19 @@
+/**
+ * FeedController
+ *
+ * @description :: Server-side logic for managing Feeds
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	subscribe: function(req, res) {
+    if(!req.isSocket) {
+      return res.badRequest();
+    }
+
+    sails.sockets.join(req.socket, 'feed');
+
+    return res.ok();
+  }
+};
+
